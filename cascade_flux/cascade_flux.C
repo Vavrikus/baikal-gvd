7c1e9f66e71e1b5899842a7aee969cc03aaf7ec2
Martin Vavrik <martinvitvavrik@gmail.com>
2021-05-13 18:19:52 +0200

    Better multiple events coincidence output

--- cascade_flux/cascade_flux.C
+++ cascade_flux/cascade_flux.C
@@ -27,7 +27,7 @@ using namespace std;
 double xPos[40] = {-13.76,32.14,45.06,5.13,-45.03,-76.21,-59.85,-14.47,-195.19,-164.79,-180.08,-227.51,-276.24,-279.59,-248.17,-222.70,-270.25,-228.58,-220.89,-261.89,-309.86,-337.48,-319.74,-282.27,65.85,108.73,113.87,74.19,25.1,-2.48,16.08,58.37,-163.91,-119.26,-113.90,-152.28,-202.59,-230.83,-213.25,-170.30};
 double yPos[40] = {-211.35,-235.88,-285.45,-325.83,-319.82,-281.63,-231.37,-270.17,-340.62,-384.09,-435.13,-450.13,-424.31,-372.59,-337.03,-391.09,-37.36,-65.26,-117.78,-153.57,-146.26,-101.43,-55.24,-96.82,-435.47,-462.39,-514.68,-549.90,-544.25,-500.53,-453,-491.97,-628.26,-656.49,-707.52,-744.24,-738.58,-694.13,-645.06,-685.35};
 
-vector<vector<vector<int>>> ledMatrixRuns = {{{2,3,4,5,6,7,8,9,10,11,118,119,177,193,194,200,201,228,229,230,231,232,233,234,235,236,237,560,598}},{{},{}},{{7,117,412,429,443,459,474,490,505,520,548,564,579,595},{1,2,3,6,7,37,134,340,428,450,464,480,495,510,527,540,568,584,599,615,631,647,668},{35,36,117,120,131,151,412,429,443,459,474,489,504,519,520,547,575,591,607,623,644}},{{17,18,37,38,39,40,44,61,77,93,97,111,126,142,158,174,190,203,218,232,247,264,277,292,362,377,392,407,422,437,452,467,484,536,551,566,583,596,611,628,644,661,676,677,693},{8,41,54,56,60,61,77,92,107,123,138,154,169,184,201,215,231,245,260,276,306,375,391,406,421,436,451,466,481,498,553,571,586,603,616,631,648,663,679,694,709},{8,9,10,24,80,93,109,124,139,155,170,185,201,216,233,247,262,276,291,329,330,331,337,406,422,437,453,468,483,498,513,530,594,595,596,597,611,612,629,642,657,674,689,705,720,735},{13,23,36,51,67,82,100,116,131,146,162,179,193,208,222,237,251,268,283,350,367,384},{13,23,34,50,67,82,86,88,89,90,91,92,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,112,113,114,116,117,118,120,121,122,123,124,129,130,132,137,147,163,180,193,208,222,237,238,253,265,279,363,379}},{{3,19,32,42,51,52,62,71,82,92,102,122,145,156,165,180},{12,14,24,33,35,42,51,60,69,83,90,111,134,145,146,147,155,156,157,158,159,160,162,164},{9,13,14,17,132,143,153,164,165,167,169,172},{1,15,17,21,26,36,46,58,67,76,86,94,103,112,114},{2,12,17,19,23,24,26,36,44,55,63,73,82,89,98,106,117,131,143,151,160,166,168,175},{18,20,25,31,41,51,62,71,90,110,118,128,130,143,145,154,157,163,166,173,178,185,195,220,232,241,250,260,282,296,301,312,326,336,346,356,367,384,394,404,414,425,434,442,451},{7,10,12,16,17,22,30,40,49,58,67,76,84,93,102,105,113,115,129,131,143,144,149,152,159,165,169,174,177,207,219,228,237,245,254,264,277,281,290,301,312,322,332,342,359,369,380,389,398,407,417,426}}};
+vector<vector<vector<int>>> ledMatrixRuns = {{{}},{{},{}},{{},{},{}},{{},{},{},{},{}},{{},{},{},{},{},{},{}}};;//{{{2,3,4,5,6,7,8,9,10,11,118,119,177,193,194,200,201,228,229,230,231,232,233,234,235,236,237,560,598}},{{},{}},{{7,117,412,429,443,459,474,490,505,520,548,564,579,595},{1,2,3,6,7,37,134,340,428,450,464,480,495,510,527,540,568,584,599,615,631,647,668},{35,36,117,120,131,151,412,429,443,459,474,489,504,519,520,547,575,591,607,623,644}},{{17,18,37,38,39,40,44,61,77,93,97,111,126,142,158,174,190,203,218,232,247,264,277,292,362,377,392,407,422,437,452,467,484,536,551,566,583,596,611,628,644,661,676,677,693},{8,41,54,56,60,61,77,92,107,123,138,154,169,184,201,215,231,245,260,276,306,375,391,406,421,436,451,466,481,498,553,571,586,603,616,631,648,663,679,694,709},{8,9,10,24,80,93,109,124,139,155,170,185,201,216,233,247,262,276,291,329,330,331,337,406,422,437,453,468,483,498,513,530,594,595,596,597,611,612,629,642,657,674,689,705,720,735},{13,23,36,51,67,82,100,116,131,146,162,179,193,208,222,237,251,268,283,350,367,384},{13,23,34,50,67,82,86,88,89,90,91,92,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,112,113,114,116,117,118,120,121,122,123,124,129,130,132,137,147,163,180,193,208,222,237,238,253,265,279,363,379}},{{3,19,32,42,51,52,62,71,82,92,102,122,145,156,165,180},{12,14,24,33,35,42,51,60,69,83,90,111,134,145,146,147,155,156,157,158,159,160,162,164},{9,13,14,17,132,143,153,164,165,167,169,172},{1,15,17,21,26,36,46,58,67,76,86,94,103,112,114},{2,12,17,19,23,24,26,36,44,55,63,73,82,89,98,106,117,131,143,151,160,166,168,175},{18,20,25,31,41,51,62,71,90,110,118,128,130,143,145,154,157,163,166,173,178,185,195,220,232,241,250,260,282,296,301,312,326,336,346,356,367,384,394,404,414,425,434,442,451},{7,10,12,16,17,22,30,40,49,58,67,76,84,93,102,105,113,115,129,131,143,144,149,152,159,165,169,174,177,207,219,228,237,245,254,264,277,281,290,301,312,322,332,342,359,369,380,389,398,407,417,426}}};
 
 int unix1995 = 788918400;
 
@@ -60,6 +60,21 @@ struct Event
 
 vector<Event> sortedEvents;
 int nCoincidences = 0;
+
+struct Coincidence
+{
+	int m_numOfEvents;
+	vector<Event> m_events;
+
+	Coincidence& copy()
+	{
+		Coincidence* c;
+		c->m_numOfEvents = this->m_numOfEvents;
+		c->m_events = this->m_events;
+
+		return *c;
+	}
+};
 
 //getting data from global variables to objects
 void ParseEvent(Event& ev)
@@ -100,43 +115,61 @@ void ParseEvent(Event& ev)
 //operator overloading for printing
 std::ostream& operator<<(std::ostream& stream, const Event& ev)
 {
-	stream << "#############################################  EVENT INFO  #############################################\n";
-	stream << "Time Stamp:\n     ";
+	stream << "     ======================================  EVENT INFO  ======================================\n";
+	stream << "     Time Stamp:\n          ";
 	ev.m_eventTime.Print();
 
-	stream << "\nIDs:\n     ";
-    stream << "eventID: " << ev.m_eventID << ", seasonID: " << ev.m_seasonID << ", clusterID: ";
+	stream << "\n     IDs:\n     ";
+    stream << "     eventID: " << ev.m_eventID << ", seasonID: " << ev.m_seasonID << ", clusterID: ";
     stream << ev.m_clusterID << ", runID: " << ev.m_runID << "\n\n";
 
-    stream << "Reconstructed variables:\n     ";
-    stream << "energy = " << ev.m_energy << " TeV, sigma = " << ev.m_energySigma << " TeV\n     ";
-    stream << "theta = " << ev.m_theta/TMath::Pi()*180 << ", sigma = " << ev.m_thetaSigma/TMath::Pi()*180;
+    stream << "     Reconstructed variables:\n     ";
+    stream << "     energy = " << ev.m_energy << " TeV, sigma = " << ev.m_energySigma << " TeV\n     ";
+    stream << "     theta = " << ev.m_theta/TMath::Pi()*180 << ", sigma = " << ev.m_thetaSigma/TMath::Pi()*180;
     stream << ", phi = " << ev.m_phi/TMath::Pi()*180 << ", sigma = " << ev.m_phiSigma/TMath::Pi()*180;
     stream << ", direction sigma = " << ev.m_directionSigma << "\n     ";
-    stream << "Position (XYZ): " << ev.m_position.X();
+    stream << "     Position (XYZ): " << ev.m_position.X();
     stream << " " << ev.m_position.Y() << " " << ev.m_position.Z() << "\n     ";
-    stream << "right ascension = " << ev.m_rightAscension/TMath::Pi()*180;
+    stream << "     right ascension = " << ev.m_rightAscension/TMath::Pi()*180;
     stream << ", declination = " << ev.m_declination/TMath::Pi()*180 << "\n\n";
 
-    stream << "Reconstruction parameters:\n     ";
-    stream << "nHits = " << ev.m_nHits << ", nTrackHits = " << ev.m_nTrackHits << ", qTotal = ";
+    stream << "     Reconstruction parameters:\n     ";
+    stream << "     nHits = " << ev.m_nHits << ", nTrackHits = " << ev.m_nTrackHits << ", qTotal = ";
     stream << ev.m_qTotal << "\n     ";
-    stream << "likelihood = " << ev.m_likelihood << ", likelihoodHitOnly = " << ev.m_likelihoodHitOnly;
-    stream << "\n     cascTime = " << ev.m_cascTime << "\n   ";
+    stream << "     likelihood = " << ev.m_likelihood << ", likelihoodHitOnly = " << ev.m_likelihoodHitOnly;
+    stream << "\n          cascTime = " << ev.m_cascTime << "\n   ";
 
-    stream << "After TFilter:\n     ";
-    stream << "nHits = " << ev.m_nHitsAfterTFilter <<  ", nStrings = " << ev.m_nStringsAfterTFilter;
+    stream << "     After TFilter:\n     ";
+    stream << "     nHits = " << ev.m_nHitsAfterTFilter <<  ", nStrings = " << ev.m_nStringsAfterTFilter;
     stream << ", chi2 = " << ev.m_chi2AfterTFilter << "\n   ";
 
-    stream << "After Caus:\n     ";
-    stream << "nHits = " << ev.m_nHitsAfterCaus << ", nStrings = " << ev.m_nStringsAfterCaus;
+    stream << "     After Caus:\n     ";
+    stream << "     nHits = " << ev.m_nHitsAfterCaus << ", nStrings = " << ev.m_nStringsAfterCaus;
     stream << ", chi2 = " << ev.m_chi2AfterCaus << "\n\n";
 
-    stream << "MC data:\n     ";
-    stream << "energy = " << ev.m_mcEnergy << " TeV, theta = " << ev.m_mcTheta/TMath::Pi()*180; 
+    stream << "     MC data:\n     ";
+    stream << "     energy = " << ev.m_mcEnergy << " TeV, theta = " << ev.m_mcTheta/TMath::Pi()*180; 
     stream << ", phi = " << ev.m_mcPhi/TMath::Pi()*180 << "\n     ";
-    stream << "Position (XYZ): " << ev.m_mcPosition.X();
+    stream << "     Position (XYZ): " << ev.m_mcPosition.X();
     stream << " " << ev.m_mcPosition.Y() << " " << ev.m_mcPosition.Z() << "\n";
+
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Coincidence& c)
+{
+	stream << "########################################  COINCIDENCE  #######################################\n";
+	stream << "Number of events: " << c.m_numOfEvents << "\n\n";
+
+	stream << "Time differences: ";
+	for(int i = 1; i < c.m_numOfEvents; i++)
+	{
+		if(i!=1) cout << ", ";
+		stream << c.m_events[i].m_eventTime.GetSec()-c.m_events[i-1].m_eventTime.GetSec(); 
+	} 
+	stream << "\n\n";
+
+	for(auto const& ev : c.m_events) stream << ev << "\n\n";
 
     return stream;
 }
@@ -285,24 +318,52 @@ void QuickSort(vector<Event>& arr, int high = -1, int low = 0)
 //writes warning if two cascades are separated by smaller than selected amount of time
 void WarnIfCloser(const vector<Event>& arr, long int minSec)
 {
+	vector<Coincidence> coincidences;
+
 	if(arr.size() > 0)
 	{
 		long int previousTime = arr[0].m_eventTime.GetSec();
 
+		Coincidence c;
+
+		bool IsCoincidence = false;
+
 		for(int i = 1; i < arr.size(); i++)
 		{
-			if(arr[i].m_eventTime.GetSec() - previousTime < minSec)
-			{
-				cout << "\nEvents with time difference " << arr[i].m_eventTime.GetSec() - previousTime;
-				cout << " seconds (smaller than " << minSec << "):\n\n";
-				cout << arr[i-1] << "\n\n" << arr[i] << "\n";
+			if(arr[i].m_eventTime.GetSec() - previousTime <= minSec)
+			{	
+				if(!IsCoincidence)
+				{
+					nCoincidences++;
 
-				nCoincidences++;
+					IsCoincidence = true;
+
+					c.m_numOfEvents = 2;
+					c.m_events.clear();
+					c.m_events.push_back(arr[i-1]);
+					c.m_events.push_back(arr[i]);
+				}
+
+				else
+				{
+					c.m_numOfEvents++;
+					c.m_events.push_back(arr[i]);
+				}
 			}
+
+			else
+			{
+				if(IsCoincidence) coincidences.push_back(c);
+				IsCoincidence = false;
+			} 
 
 			previousTime = arr[i].m_eventTime.GetSec();
 		}
+		if(IsCoincidence) coincidences.push_back(c);
 	}
+
+	cout << "\n\n";
+	for(auto const& c : coincidences) cout << c;
 }
 
 int cascade_flux(int val = 0, int year = -1, int cluster = -1)
