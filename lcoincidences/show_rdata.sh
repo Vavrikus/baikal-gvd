4c22a3b7cb79848cd5eafec4addb7d599899c56c
Martin Vavrik <martinvitvavrik@gmail.com>
2021-11-29 16:13:10 +0100

    Showing local data count.

--- lcoincidences/show_rdata.sh
+++ lcoincidences/show_rdata.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 cd $(dirname $0)
 
-while getopts "S:a" opt; do
+while getopts "S:al" opt; do
     case $opt in
         S)  set -f # disable glob
             IFS=',' # split on comma characters
             array=($OPTARG) ;; # use the split+glob operator
         a)	array=("aries" "perseus" "orion" "lyra"
         	"dragon" "cassiopeia" "gidra" "hercules" "ursa" "val") ;;
+        l)  echo "Localhost:"
+            ls -1 data/data* | wc -l ;;
     esac
 done
 
