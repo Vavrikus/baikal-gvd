65b812982fabed865c32dc9d1ccd9bfecb4b8d6b
Martin Vavrik <martinvitvavrik@gmail.com>
2022-03-02 15:11:33 +0100

    Drawing iterated data prototype.

--- lcoincidences/get_prob.C
+++ lcoincidences/get_prob.C
@@ -64,13 +64,23 @@ public:
 
 };
 
-int get_prob()
+void ReadAndFill1D(double dec, double ra, TH1F* prob_hist, TH1F* prob_hist2)
 {
-	TH1F* prob_hist  = new TH1F("prob_hist","Test statistic distribution;Test statistic;Probability TS is bigger",10000,-1,22);
-	TH1F* prob_hist2 = new TH1F("prob_hist2","nSignal distribution;nSignal;Probability nSign is bigger",10000,-1,5);
+	string inpath, inpath2;
 
-	ifstream  inf{"./data/merged/fdata_tStat_dec_-90.000000_all.txt"};
-	ifstream inf2{"./data/merged/fdata_nSign_dec_-90.000000_all.txt"};
+	if(ra == -1000)
+	{
+		inpath  = "./data/merged/fdata_tStat_dec_" + to_string(dec) + "_all.txt";
+		inpath2 = "./data/merged/fdata_nSign_dec_" + to_string(dec) + "_all.txt";
+	}
+	else
+	{
+		inpath  = "./data/merged/fdata_tStat_dec_" + to_string(dec) + "_" + to_string(ra) + "_all.txt";
+		inpath2 = "./data/merged/fdata_nSign_dec_" + to_string(dec) + "_" + to_string(ra) + "_all.txt";		
+	}
+
+	ifstream  inf{inpath};
+	ifstream inf2{inpath2};
 
 	if(!inf||!inf2) cout << "File not found.\n";
 
@@ -109,24 +119,10 @@ int get_prob()
 	        throw;
     	}
 	}
+}
 
-	const int nodes = 50;
-	const double low = -1;
-	const double high = 33;
-	double spline_nodes[nodes];
-
-	for (int i = 0; i < nodes; ++i)
-	{
-		spline_nodes[i] = low+(high-low)*i/nodes;
-	}
-
-	NSpline<nodes>* s= new NSpline<nodes>(spline_nodes);
-	s->SetDer1(0);
-	s->SetDer2(0);
-	TF1* f_spline = new TF1("f_probfit", s->GetEval(), low, high, 2*nodes+2);
-	TF1* f_exp    = new TF1("f_probfit2", "[0]*exp(-[1]*x)",-1,50);
-	f_exp->SetParameters(1,1);
-
+void Draw1D(TH1F* prob_hist, TH1F* prob_hist2)
+{
 	TCanvas* c1 = new TCanvas("c1","TS distribution");
 
 	gStyle->SetOptStat(111111);
@@ -134,8 +130,6 @@ int get_prob()
 	gPad->SetLogy();
 	TH1* prob_hist_cumul = prob_hist->GetCumulative(kFALSE);
 	prob_hist_cumul->Scale(1./prob_hist->GetEntries());
-	prob_hist_cumul->Fit(f_spline,"M","",low,high);
-	prob_hist_cumul->Fit(f_exp,"M","",15,35);
 	prob_hist_cumul->Draw();
 
 	TCanvas* c2 = new TCanvas("c2","nS distribution");
@@ -143,13 +137,81 @@ int get_prob()
 	TH1* prob_hist2_cumul = prob_hist2->GetCumulative(kFALSE);
 	prob_hist2_cumul->Scale(1./prob_hist2->GetEntries());
 	prob_hist2_cumul->Draw();
+}
+
+int get_prob()
+{
+	THStack* hs  = new THStack("hs", "Test statistic distribution");
+	THStack* hs2 = new THStack("hs2","nSignal distribution");
+	for(double sigRa = -180; sigRa < 180; sigRa += 10)
+	{
+		TH1F* prob_hist  = new TH1F("prob_hist","Test statistic distribution;Test statistic;Probability TS is bigger",10000,-1,22);
+		TH1F* prob_hist2 = new TH1F("prob_hist2","nSignal distribution;nSignal;Probability nSign is bigger",10000,-1,5);
+		ReadAndFill1D(45,sigRa,prob_hist,prob_hist2);
+
+		TH1* prob_hist_cumul = prob_hist->GetCumulative(kFALSE);
+		prob_hist_cumul->Scale(1./prob_hist->GetEntries());
+		TH1* prob_hist2_cumul = prob_hist2->GetCumulative(kFALSE);
+		prob_hist2_cumul->Scale(1./prob_hist2->GetEntries());
+
+		hs->Add(prob_hist_cumul);
+		hs2->Add(prob_hist2_cumul);
+	}
+
+	TCanvas* c1 = new TCanvas("c1","TS distribution");
+	gStyle->SetOptStat(111111);
+	gPad->SetLogy();
+	hs->Draw("nostack");
+
+	TCanvas* c2 = new TCanvas("c2","nS distribution");
+	gPad->SetLogy();
+	hs2->Draw("nostack");
+	
+	// TH1F* prob_hist  = new TH1F("prob_hist","Test statistic distribution;Test statistic;Probability TS is bigger",10000,-1,22);
+	// TH1F* prob_hist2 = new TH1F("prob_hist2","nSignal distribution;nSignal;Probability nSign is bigger",10000,-1,5);
+	// ReadAndFill1D(-90,-1000,prob_hist,prob_hist2);
+	// Draw1D(prob_hist,prob_hist2);
+
+	// const int nodes = 50;
+	// const double low = -1;
+	// const double high = 33;
+	// double spline_nodes[nodes];
+
+	// for (int i = 0; i < nodes; ++i)
+	// {
+	// 	spline_nodes[i] = low+(high-low)*i/nodes;
+	// }
+
+	// NSpline<nodes>* s= new NSpline<nodes>(spline_nodes);
+	// s->SetDer1(0);
+	// s->SetDer2(0);
+	// TF1* f_spline = new TF1("f_probfit", s->GetEval(), low, high, 2*nodes+2);
+	// TF1* f_exp    = new TF1("f_probfit2", "[0]*exp(-[1]*x)",-1,50);
+	// f_exp->SetParameters(1,1);
+
+	// TCanvas* c1 = new TCanvas("c1","TS distribution");
+
+	// gStyle->SetOptStat(111111);
+
+	// gPad->SetLogy();
+	// TH1* prob_hist_cumul = prob_hist->GetCumulative(kFALSE);
+	// prob_hist_cumul->Scale(1./prob_hist->GetEntries());
+	// prob_hist_cumul->Fit(f_spline,"M","",low,high);
+	// prob_hist_cumul->Fit(f_exp,"M","",15,35);
+	// prob_hist_cumul->Draw();
+
+	// TCanvas* c2 = new TCanvas("c2","nS distribution");
+	// gPad->SetLogy();
+	// TH1* prob_hist2_cumul = prob_hist2->GetCumulative(kFALSE);
+	// prob_hist2_cumul->Scale(1./prob_hist2->GetEntries());
+	// prob_hist2_cumul->Draw();
 
 
-	TFile* outputFile = new TFile("prob.root","RECREATE");
-	prob_hist_cumul->Write();
-	prob_hist2_cumul->Write();
-	f_spline->Write();
-	f_exp->Write();
+	// TFile* outputFile = new TFile("prob.root","RECREATE");
+	// prob_hist_cumul->Write();
+	// prob_hist2_cumul->Write();
+	// f_spline->Write();
+	// f_exp->Write();
 
 	return 0;
 }
